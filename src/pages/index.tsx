090592a39c307685f050f0e2fe556b38a9a80be7
Sho0226 <s1F102201616@iniad.org>
2024-05-08 22:57:34 +0900

    board

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [samplePos, setSamplePos] = useState(0);
-  const bombCount - 10;
+  const bombCount = 10;
   // 0 -> ボム無し
   // 1 -> ボム有り
   const [bombMap, setBombMap] = useState([
@@ -33,30 +33,36 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-const isPlaying = userIn.some ((row) => row.some((input) => input !== 0));
-const isFailure = userIn.some ((row, y) =>
-  row.some((input, x) => input === 1 && bombMap[y][x] === 1)
-);
+  const isPlaying = userIn.some((row) => row.some((input) => input !== 0));
+  const isFailure = userIn.some((row, y) =>
+    row.some((input, x) => input === 1 && bombMap[y][x] === 1),
+  );
 
-// -1   -> 石
-//  0   -> 画像無しセル
-//  1~8 -> 数字セル
-//  9   -> 石+はてな
-//  10  ->石+旗
-//  11  -> ボムセル
-
+  // -1   -> 石
+  //  0   -> 画像無しセル
+  //  1~8 -> 数字セル
+  //  9   -> 石+はてな
+  //  10  ->石+旗
+  //  11  -> ボムセル
 
   const board: number[][] = [];
 
   console.log(samplePos);
   return (
     <div className={styles.container}>
+       <div className={styles.boardstyle}>
+       {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cellstyle} key={`${x}-${y}`} onClick={() => bombMap(x, y)}>
+    <div className={styles.sellstyle}>
+
+    </div>
       <div
         className={styles.samplestyle}
         style={{ backgroundPosition: `${-30 * samplePos}px 0px` }}
       />
       <button onClick={() => setSamplePos((P) => (P + 1) % 14)}>sample</button>
-    </div>
+    </div></div>
   );
 };
 
