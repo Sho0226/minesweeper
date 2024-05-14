c2edce9d927a7e16e95307aa5b6b7771dfadde8f
Sho0226 <s1F102201616@iniad.org>
2024-05-15 01:25:37 +0900

    play/fail out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -158,7 +158,7 @@ const Home = () => {
   // const isPlaying = userIn.some((row) => row.some((input) => input !== 0));
   // const isFailure = userIn.some((row, y) =>
   //   row.some((input, x) => input === 1 && bombMap[y][x] === 1),
-  );
+  // );
 
   // -1   -> 石
   //  0   -> 画像無しセル
